08c5fffcb82138ac062876cd6c7c0799d3a583e6
Julian Krings <juliankrings6@gmail.com>
2025-05-14 20:34:22 +0200

    update workflows and disable license checking for now

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,12 @@ subprojects {
         testRuntimeOnly(rootProject.libs.jupiter.engine)
     }
 
+    tasks {
+        listOf("applyLicenses", "checkLicenses", "checkLicenseMain", "checkLicenseTest").forEach {
+            named(it) { enabled = false }
+        }
+    }
+
     extensions.findByType<MavenPublishBaseExtension>()?.apply {
         publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
         signAllPublications()
